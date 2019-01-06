c2ad5070d28b5e8f997a51f56d4351f3e450251a
Alex <ajollid@gmail.com>
2019-01-07 04:59:50 +0800

    Add first brancH

--- fractal-tree.js
+++ fractal-tree.js
@@ -12,12 +12,26 @@ ctx.fillRect(0, 0, SIZE, SIZE);
 const origin = {
   x: SIZE / 2,
   y: SIZE,
+  length: 400 * (2 / 3),
 };
 
 ctx.beginPath();
 ctx.moveTo(origin.x, origin.y);
-ctx.lineTo(origin.x, origin.y * (2 / 3));
+origin.y *= (2 / 3);
+origin.length *= (2 / 3);
+ctx.lineTo(origin.x, origin.y);
 ctx.strokeStyle = 'white';
 ctx.stroke();
 ctx.closePath();
 
+ctx.beginPath();
+ctx.moveTo(origin.x, origin.y);
+origin.length *= (2 / 3);
+
+origin.x += Math.cos(Math.PI / 4) * origin.length;
+origin.y -= Math.sin(Math.PI / 4) * origin.length;
+
+ctx.lineTo(origin.x, origin.y);
+ctx.strokeStyle = 'white';
+ctx.stroke();
+ctx.closePath();
