566cdf221e02b65b6acb40b0768378677faa9d70
Alex <ajhollid@gmail.com>
2019-01-09 12:10:37 -0800

    Add recursive branch function

--- fractal-tree.js
+++ fractal-tree.js
@@ -1,39 +1,37 @@
 const canvas = document.getElementById('fractal_canvas');
 const ctx = canvas.getContext('2d');
 
-const SIZE = 400;
-
-ctx.canvas.width = SIZE;
-ctx.canvas.height = SIZE;
+const HEIGHT = 800;
+const WIDTH = 1200;
+ctx.canvas.width = WIDTH;
+ctx.canvas.height = HEIGHT;
 
 ctx.fillStyle = '#777777';
-ctx.fillRect(0, 0, SIZE, SIZE);
+ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-function draw() {
+const origin = {
+  x: WIDTH / 2,
+  y: HEIGHT,
+  length: HEIGHT * (1 / 3),
+  angle: -Math.PI / 2,
+};
+
+
+function drawBranch(x, y, a, l) {
+  const newX = x + Math.cos(a) * l;
+  const newY = y + Math.sin(a) * l;
+
   ctx.beginPath();
-  ctx.moveTo(origin.x, origin.y);
-  origin.length *= (2 / 3);
-
-  if (origin.angle === Math.PI) {
-    origin.y = origin.length;
-  } else {
-    origin.x += Math.cos(origin.angle % Math.PI) * origin.length;
-    origin.y -= Math.sin(origin.angle % Math.PI) * origin.length;
-  }
-
-  ctx.lineTo(origin.x, origin.y);
-  console.log(origin.x, origin.y);
+  ctx.moveTo(x, y);
+  ctx.lineTo(newX, newY);
   ctx.strokeStyle = 'white';
   ctx.stroke();
   ctx.closePath();
+  if (l > 4) {
+    drawBranch(newX, newY, a - (Math.PI / 8), l * (2 / 3));
+    drawBranch(newX, newY, a + (Math.PI / 8), l * (2 / 3));
+  }
 }
 
-const origin = {
-  x: SIZE / 2,
-  y: SIZE,
-  length: 400 * (2 / 3),
-  angle: Math.PI,
-};
-
-draw();
+drawBranch(origin.x, origin.y, origin.angle, origin.length);
 
