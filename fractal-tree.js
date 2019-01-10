3bf896054bddb6522127184bff24cf9cdb6f5778
Alex <ajhollid@gmail.com>
2019-01-10 15:52:06 -0800

    Add random factor to angles, length.  Add iteration limit to drawBranch

--- fractal-tree.js
+++ fractal-tree.js
@@ -9,33 +9,47 @@ ctx.canvas.height = HEIGHT;
 ctx.fillStyle = '#777777';
 ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-const origin = {
-  x: WIDTH / 2,
-  y: HEIGHT,
-  length: HEIGHT * (1 / 3),
-  angle: -Math.PI / 2,
-};
-
 function randomFactor(min, max) {
   return min + (max - min) * Math.random();
 }
 
+const origin = {
+  x: WIDTH / 2,
+  y: HEIGHT,
+  length: HEIGHT * randomFactor(0.1, 0.3),
+  angle: -Math.PI / 2,
+  angleIncrement: Math.PI / 8,
+  randomAngleMax: 1.5,
+};
 
-function drawBranch(x, y, a, l) {
-  const newX = x + Math.cos(a) * l;
-  const newY = y + Math.sin(a) * l;
 
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(newX, newY);
-  ctx.strokeStyle = 'white';
-  ctx.stroke();
-  ctx.closePath();
-  if (l > 4) {
-    drawBranch(newX, newY, a - (Math.PI / 8), l * (2 / 3));
-    drawBranch(newX, newY, a + (Math.PI / 8), l * (2 / 3));
+function drawBranch(x, y, a, l, count) {
+  if (count < 10) {
+    const newCount = count + 1;
+    const newX = x + Math.cos(a) * l;
+    const newY = y + Math.sin(a) * l;
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(newX, newY);
+    ctx.strokeStyle = 'white';
+    ctx.stroke();
+    ctx.closePath();
+
+    drawBranch(
+      newX, newY,
+      a - origin.angleIncrement * randomFactor(1, origin.randomAngleMax),
+      l * randomFactor(0.5, 0.8),
+      newCount,
+    );
+    drawBranch(
+      newX, newY,
+      a + origin.angleIncrement * randomFactor(1, origin.randomAngleMax),
+      l * randomFactor(0.5, 0.8),
+      newCount,
+    );
   }
 }
 
-drawBranch(origin.x, origin.y, origin.angle, origin.length);
+drawBranch(origin.x, origin.y, origin.angle, origin.length, 0);
 
