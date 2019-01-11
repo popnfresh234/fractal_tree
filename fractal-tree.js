19c0439b9923e9fa7abe37e075d2e3df30c2fd03
Alex <ajhollid@gmail.com>
2019-01-11 09:47:48 -0800

    Use requestAnimationFrame to animate drawing trees

--- fractal-tree.js
+++ fractal-tree.js
@@ -25,34 +25,64 @@ const origin = {
 };
 
 
+function buildWaypoints(startX, startY, endX, endY) {
+  const frameRate = 60;
+  const waypoints = [];
+  const dx = endX - startX;
+  const dy = endY - startY;
+  for (let i = 0; i < frameRate + 2; i++) {
+    const x = startX + dx * i / frameRate;
+    const y = startY + dy * i / frameRate;
+    waypoints.push({ x, y });
+  }
+  return waypoints;
+}
+
+
 function drawBranch(x, y, a, l, strokeWidth, count) {
+  let frameCount = 1;
+  let waypoints = [];
+
+  function drawLine(callback) {
+    function animateLine() {
+      ctx.beginPath();
+      ctx.moveTo(waypoints[frameCount - 1].x, waypoints[frameCount - 1].y);
+      ctx.lineTo(waypoints[frameCount].x, waypoints[frameCount].y);
+      ctx.strokeStyle = 'white';
+      ctx.lineWidth = strokeWidth;
+      ctx.stroke();
+      ctx.closePath();
+      frameCount++;
+      if (frameCount < waypoints.length - 1) {
+        requestAnimationFrame(animateLine);
+      } else {
+        callback();
+      }
+    }
+    animateLine();
+  }
+
   if (count <= 10) {
     const newCount = count + 1;
     const newX = x + Math.cos(a) * l;
     const newY = y + Math.sin(a) * l;
-
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(newX, newY);
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = strokeWidth;
-    ctx.stroke();
-    ctx.closePath();
-
-    drawBranch(
-      newX, newY,
-      a - origin.angleIncrement * randomFactor(1, origin.randomAngleMax),
-      l * randomFactor(0.5, 0.9),
-      strokeWidth * 0.7,
-      newCount,
-    );
-    drawBranch(
-      newX, newY,
-      a + origin.angleIncrement * randomFactor(1, origin.randomAngleMax),
-      l * randomFactor(0.5, 0.9),
-      strokeWidth * 0.7,
-      newCount,
-    );
+    waypoints = buildWaypoints(x, y, newX, newY);
+    drawLine(() => {
+      drawBranch(
+        newX, newY,
+        a - origin.angleIncrement * randomFactor(1, origin.randomAngleMax),
+        l * randomFactor(0.5, 0.9),
+        strokeWidth * 0.7,
+        newCount,
+      );
+      drawBranch(
+        newX, newY,
+        a + origin.angleIncrement * randomFactor(1, origin.randomAngleMax),
+        l * randomFactor(0.5, 0.9),
+        strokeWidth * 0.7,
+        newCount,
+      );
+    });
   } else {
     ctx.beginPath();
     ctx.arc(x, y, 1, 0, 2 * Math.PI);
@@ -61,7 +91,7 @@ function drawBranch(x, y, a, l, strokeWidth, count) {
   }
 }
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 10; i++) {
   drawBranch(randomFactor(400, WIDTH - 400), origin.y, origin.angle, HEIGHT * randomFactor(0.09, 0.2), randomFactor(1, 10), 0);
 }
 
